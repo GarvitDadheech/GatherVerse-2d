a06af4bb25d3e33b4c7ece71826e968a49383a0e
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-12-09 00:05:24 +0530

    add element and avatar apis

--- apps/http/src/routes/v1/avatar.ts
+++ apps/http/src/routes/v1/avatar.ts
@@ -2,3 +2,21 @@ import { Router } from "express";
 import client from "@repo/db";
 
 export const avatarRouter = Router();
+
+avatarRouter.get("/all",async (req,res) => {
+    try {
+        const avatars = await client.avatar.findMany();
+        res.json({avatars : avatars.map(avatar => ({
+                    id: avatar.id,
+                    imageUrl: avatar.imageUrl,
+                    name: avatar.name
+                })
+            )
+        })
+    }
+    catch(e) {
+        res.status(400).json({
+            message : "Internal Server Error!"
+        })
+    }
+})
