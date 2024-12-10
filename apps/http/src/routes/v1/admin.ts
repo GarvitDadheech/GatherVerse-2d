15d1f9bd000b91c940899a9217d8833e05355fc0
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-12-11 00:31:00 +0530

    add admin apis

--- apps/http/src/routes/v1/admin.ts
+++ apps/http/src/routes/v1/admin.ts
@@ -1,21 +1,142 @@
 import { Router } from "express";
-import { CreateElementSchema, UpdateElementSchema } from "../../types";
+import { CreateAvatarSchema, CreateElementSchema, CreateMapSchema, UpdateElementSchema } from "../../types";
 import { AdminMiddleware } from "../../middleware/admin";
 import client from "@repo/db";
-import { ParseStatus } from "zod";
 
 export const adminRouter = Router();
 
 adminRouter.use(AdminMiddleware);
 
 adminRouter.post("/element", async (req,res) => {
+    const parsedData = CreateElementSchema.safeParse(req.body);
+
+    if(!parsedData.success) {
+        res.status(400).json({
+            message : "Invalid Data Recieved!"
+        })
+        return;
+    }
+
+    try {
+        const element = await client.element.create({
+            data: {
+                imageUrl : parsedData.data.imageUrl,
+                width : parsedData.data.width,
+                height : parsedData.data.height,
+                static : parsedData.data.static
+            }
+        })
+
+        res.json({
+            elementId : element.id
+        })
+
+    }
+    catch(e) {
+        res.status(400).json({
+            message : "Internal Server Error!"
+        })
+    }
+})
+
+adminRouter.put("/element/:elementId",async (req,res) => {
+    const parsedData = UpdateElementSchema.safeParse(req.body);
+
+    if(!parsedData.success) {
+        res.status(400).json({
+            message : "Invalid Data Recieved!"
+        })
+        return;
+    }
+
+    try {
+        const element = await client.element.update({
+            where: {
+                id : req.params.elementId
+            },
+            data: {
+                imageUrl : parsedData.data.imageUrl,
+            }
+        })
+
+        res.json({
+            message: "Element Updated!"
+        })
+
+    }
+    catch(e) {
+        res.status(400).json({
+            message : "Internal Server Error!"
+        })
+    }
+
     
 })
 
-adminRouter.post("/avatar",(req,res) => {
+adminRouter.post("/avatar",async (req,res) => {
+    const parsedData = CreateAvatarSchema.safeParse(req.body);
+
+    if(!parsedData.success) {
+        res.status(400).json({
+            message : "Invalid Data Recieved!"
+        })
+        return;
+    }
+
+    try {
+        const avatar = await client.avatar.create({
+            data: {
+                imageUrl : parsedData.data.imageUrl,
+                name : parsedData.data.name
+            }
+        })
+
+        res.json({
+            avatarId : avatar.id
+        })
+    }
+    catch(e) {
+        res.status(400).json({
+            message : "Internal Server Error!"
+        })
+    }
     
 })
 
-adminRouter.post("/map",(req,res) => {
-    
+adminRouter.post("/map",async (req,res) => {
+    const parsedData = CreateMapSchema.safeParse(req.body);
+
+    if(!parsedData.success) {
+        res.status(400).json({
+            message : "Invalid Data Recieved!"
+        })
+        return;
+    }
+
+    try {
+        const map = await client.map.create({
+            data: {
+                name : parsedData.data.name,
+                thumbnail : parsedData.data.thumbnail,
+                width : Number(parsedData.data.dimensions.split("x")[1]),
+                height : Number(parsedData.data.dimensions.split("x")[0]),
+                mapElements : {
+                    create : parsedData.data.defaultElements.map(element => ({
+                        elementId : element.elementId,
+                        x : element.x,
+                        y : element.y
+                    }))
+                }
+            }
+        })
+
+        res.json({
+            mapId : map.id
+        })
+    }
+    catch(e) {
+        res.status(400).json({
+            message : "Internal Server Error!"
+        })
+    }
 })
