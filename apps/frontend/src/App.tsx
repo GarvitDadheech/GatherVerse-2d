5f011410b9c52b394e8a1b07863180eb2ce8b98a
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2025-01-13 01:28:15 +0530

    remove unnecessary roomSelection component

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Navigate, Route } from "react-router-dom";
 import OnboardingPage from "./pages/Onboarding";
 import PublicRooms from "./pages/PublicRooms";
 import Room from "./pages/Room";
-import RoomSelection from "./pages/roomSelection";
 import { WebSocketProvider } from "./components/providers/WebsocketProvider";
 
 const App: React.FC = () => {
@@ -13,7 +12,6 @@ const App: React.FC = () => {
           <Route path="/" element={<Navigate to="/onboarding" />} />
           <Route path="/onboarding" element={<OnboardingPage />} />
           <Route path="/public-rooms" element={<PublicRooms />} />
-          <Route path="/room" element={<RoomSelection/>}/>
           <Route path="/room/:roomId" element={<Room />} />
         </Routes>
       </BrowserRouter>
